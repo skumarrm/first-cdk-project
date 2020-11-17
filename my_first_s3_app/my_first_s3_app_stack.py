af9d82e75fff0d70eb1f1fd3cbd2ffc8f6582118
Senthil Kumarr <senthil.kumar3@ibm.com>
2020-11-17 18:40:38 +0530

    version 1 update

--- my_first_s3_app/my_first_s3_app_stack.py
+++ my_first_s3_app/my_first_s3_app_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     aws_s3 as _s3,
+    aws_iam as _iam,
     core
 )
 
@@ -8,6 +9,17 @@ class MyFirstS3AppStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-        bucket=_s3.Bucket(self, "Mybucketid")
+        bucket=_s3.Bucket(self, "Mybucketid",versioned=True
+                          ,encryption=_s3.BucketEncryption.KMS_MANAGED)
         bucket.s3_url_for_object(key=None)
         # The code that defines your stack goes here
+
+        output=core.CfnOutput(
+            self,
+            "MyBucketOutput",
+            value=bucket.bucket_name,
+            description="My first cdk Bucket",
+            export_name="MyBucketOutput"
+            )
+
+        _iam.Group(self,"gid")
