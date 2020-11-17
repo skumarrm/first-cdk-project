5e628169d0a999f7fd333d35dcb4135a86721877
Senthil Kumarr <senthil.kumar3@ibm.com>
2020-11-17 17:09:02 +0530

    Add he firs CDK project

--- my_first_s3_app/my_first_s3_app_stack.py
+++ my_first_s3_app/my_first_s3_app_stack.py
@@ -1,9 +1,13 @@
-from aws_cdk import core
+from aws_cdk import (
+    aws_s3 as _s3,
+    core
+)
 
 
 class MyFirstS3AppStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-
+        bucket=_s3.Bucket(self, "Mybucketid")
+        bucket.s3_url_for_object(key=None)
         # The code that defines your stack goes here
